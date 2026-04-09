96b719ffa03c7eb36e928f1cd80d62b9a6a80f9c
Logan Chu <logan.chu@duke.edu>
2026-04-08 22:38:22 -0400

    updated context window to be bigger to simulate real workflows

--- profiler/profile_colab.py
+++ profiler/profile_colab.py
@@ -289,7 +289,7 @@ def main():
     """Run all profiling benchmarks."""
     device = torch.device("cuda")
     dtype  = torch.float16
-    N_CTX  = 2
+    N_CTX  = 8
     N      = 64
     NUM_ODE_STEPS = 4
     NUM_STEPS_BS1 = 8
